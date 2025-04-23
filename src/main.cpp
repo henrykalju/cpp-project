442b15127b0fcbdde4439b628c8337579201e274
Henry Kaljulaid <henry.kaljulaid@gmail.com>
2025-04-23 21:30:22 +0300

    convert indents to tab

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,24 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({600, 600}), "SFML 3 Circle");
+	sf::RenderWindow window(sf::VideoMode({600, 600}), "SFML 3 Circle");
 
-    // Create a circle shape
-    sf::CircleShape circle(200.f); // radius
-    circle.setFillColor(sf::Color::Black);
-    circle.setPosition({100.f, 100.f}); // x, y
+	// Create a circle shape
+	sf::CircleShape circle(200.f); // radius
+	circle.setFillColor(sf::Color::Black);
+	circle.setPosition({100.f, 100.f}); // x, y
 
-    while (window.isOpen()) {
-        while (const std::optional event = window.pollEvent()) {
-            if (event->is<sf::Event::Closed>()) {
-                window.close();
-            }
-        }
+	while (window.isOpen()) {
+		while (const std::optional event = window.pollEvent()) {
+			if (event->is<sf::Event::Closed>()) {
+				window.close();
+			}
+		}
 
-        window.clear(sf::Color::White);
-        window.draw(circle);
-        window.display();
-    }
+		window.clear(sf::Color::White);
+		window.draw(circle);
+		window.display();
+	}
 
-    return 0;
+	return 0;
 }
