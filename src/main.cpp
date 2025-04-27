42df254412b5fe7e506eadb59d3a995ee7e6e8f9
RaidoAun <45874591+RaidoAun@users.noreply.github.com>
2025-04-27 15:09:46 +0300

    initial GUI

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,50 @@
+#include "gamestate.cpp"
 #include <SFML/Graphics.hpp>
 
-int main() {
-	sf::RenderWindow window(sf::VideoMode({600, 600}), "SFML 3 Circle");
+class GUI {
+  private:
+	sf::RenderWindow window;
+	Board board;
 
-	// Create a circle shape
-	sf::CircleShape circle(200.f); // radius
-	circle.setFillColor(sf::Color::Black);
-	circle.setPosition({100.f, 100.f}); // x, y
+  public:
+	GUI() : window(sf::RenderWindow(sf::VideoMode({600, 600}), "Game")) {}
+	void run() {
+		sf::RenderWindow window(sf::VideoMode({600, 600}), "SFML 3 Circle");
 
-	while (window.isOpen()) {
-		while (const std::optional event = window.pollEvent()) {
-			if (event->is<sf::Event::Closed>()) {
-				window.close();
+		while (window.isOpen()) {
+			while (const std::optional event = window.pollEvent()) {
+				if (event->is<sf::Event::Closed>()) {
+					window.close();
+				}
+			}
+
+			window.clear(sf::Color::White);
+			drawBoard();
+			window.display();
+		}
+	}
+	void drawBoard() {
+		for (int row = 0; row < ROW_COUNT - 2; row++) {
+			for (int col = 0; col < COL_COUNT - 2; col++) {
+				switch (board.spaces[row][col]) {
+				case SpacePlayer::Empty: {
+					sf::RectangleShape rect({10.f, 60.f});
+					rect.setPosition({100.f, 100.f});
+					rect.setFillColor(sf::Color::Black);
+					window.draw(rect);
+				}
+
+				default:
+					break;
+				};
 			}
 		}
-
-		window.clear(sf::Color::White);
-		window.draw(circle);
-		window.display();
 	}
+};
 
+int main() {
+
+	GUI gui{};
+	gui.run();
 	return 0;
 }
